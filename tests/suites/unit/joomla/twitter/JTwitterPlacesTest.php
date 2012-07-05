8c68328bdd9dc22621a7696b1580b491470510d8
dianaprajescu <diana.prajescu@gmail.com>
2012-07-05 17:49:09 +0300

    Create class for Places & Geo object.

---
+++ tests/suites/unit/joomla/twitter/JTwitterPlacesTest.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * @package     Joomla.UnitTest
+ * @subpackage  Twitter
+ *
+ * @copyright   Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+require_once JPATH_PLATFORM . '/joomla/twitter/twitter.php';
+require_once JPATH_PLATFORM . '/joomla/twitter/http.php';
+require_once JPATH_PLATFORM . '/joomla/twitter/places.php';
+
+/**
+ * Test class for JTwitterFriends.
+ *
+ * @package     Joomla.UnitTest
+ * @subpackage  Twitter
+ *
+ * @since       12.1
+ */
+class JTwitterPlacesTest extends TestCase
+{
+	/**
+	 * @var    JRegistry  Options for the Twitter object.
+	 * @since  12.1
+	 */
+	protected $options;
+
+	/**
+	 * @var    JTwitterHttp  Mock client object.
+	 * @since  12.1
+	 */
+	protected $client;
+
+	/**
+	 * @var    JTwitterPlaces  Object under test.
+	 * @since  12.1
+	 */
+	protected $object;
+
+	/**
+	 * @var    JTwitterOAuth  Authentication object for the Twitter object.
+	 * @since  12.1
+	 */
+	protected $oauth;
+
+	/**
+	 * @var    string  Sample JSON string.
+	 * @since  12.1
+	 */
+	protected $sampleString = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
+
+	/**
+	 * @var    string  Sample JSON error message.
+	 * @since  12.1
+	 */
+	protected $errorString = '{"error":"Generic error"}';
+
+	/**
+	 * @var    string  Sample JSON Twitter error message.
+	 * @since  12.1
+	 */
+	protected $twitterErrorString = '{"errors":[{"message":"Sorry, that page does not exist","code":34}]}';
+
+	/**
+	 * @var    string  Sample JSON string.
+	 * @since  12.1
+	 */
+	protected $rateLimit = '{"remaining_hits":150, "reset_time":"Mon Jun 25 17:20:53 +0000 2012"}';
+
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 *
+	 * @access protected
+	 *
+	 * @return void
+	 */
+	protected function setUp()
+	{
+		$key = "lIio7RcLe5IASG5jpnZrA";
+		$secret = "dl3BrWij7LT04NUpy37BRJxGXpWgjNvMrneuQ11EveE";
+		$my_url = "http://127.0.0.1/gsoc/joomla-platform/twitter_test.php";
+
+		$this->options = new JRegistry;
+		$this->client = $this->getMock('JTwitterHttp', array('get', 'post', 'delete', 'put'));
+
+		$this->object = new JTwitterPlaces($this->options, $this->client);
+		$this->oauth = new JTwitterOAuth($key, $secret, $my_url, $this->client);
+		$this->oauth->setToken($key, $secret);
+	}
+}
